807baac73ade6e261e7c2d89159a61331ee95f9a
rudresh-dixit <rudresh-dixit@lenovo>
2020-11-18 16:01:48 +0530

    refactor: remove dir content function in seperate file

---
+++ src/common_utils/rm_dir_content.py
@@ -0,0 +1,21 @@
+# module : to interact with the operating system
+import os
+
+# module :  for high level operations on files and collection of files
+# helps in automating the copying and removal of files and directories
+import shutil
+
+# function: to delete contents of the given folder
+def rm_dir_content(dir):
+    for filename in os.listdir(dir):
+        file_path = os.path.join(dir, filename)
+        try:
+            # if it is a file or a symbolic link, delete or unlink the file
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+                
+            # if it is a directory, remove directory tree
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
