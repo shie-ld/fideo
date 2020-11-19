8d80dc594e57654ac5fe46d1a4864bc1af47cdeb
rudresh-dixit <rudresh-dixit@lenovo>
2020-11-19 10:37:39 +0530

    fix: improved memory management in encoding part

--- src/encoding/get_bitarray.py
+++ src/encoding/get_bitarray.py
@@ -1,6 +1,8 @@
 # bitstring module :  for the creation and analysis of binary data
 from bitstring import BitArray
 
+import os
+
 # function : get binary representation of a file
 def get_bitarray(filepath):
     print("Converting " + os.path.basename(filepath) + " to binary form...")
@@ -9,6 +11,9 @@ def get_bitarray(filepath):
     bitarray = BitArray(filename = filepath)
 #     print("Type: " ,type(bitarray))
 #     print("Hexdump: ", bitarray)
+
+    # removing the gzip file after deriving bitarray from it
+    os.remove(filepath + ".gz")
     
     print("Successfully converted " + os.path.basename(filepath) + " to binary form")
 
